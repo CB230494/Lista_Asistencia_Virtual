3e79ce0471cb7dd6a2a4ff469d63dee97a74ade9
CB230494 <161616911+CB230494@users.noreply.github.com>
2026-08-17 16:02:28 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Después de implementar Code.gs como Aplicación web,
 // pegue aquí la URL que termina en /exec.
-const APPS_SCRIPT_URL = "PEGAR_AQUI_URL_WEB_APP";
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwKDgR_0GQC33kvgv-9cFBUToSmPfj5p-ycKoe0cyOStuF1yuALse-Zovk4oyY7Q2U_UA/exec";
